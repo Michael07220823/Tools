da27573e453f1dc2e8a2f32ec4c4b3c151cf1922
Overcomer <michael31703@gmail.com>
2020-05-28 23:12:02 +0800

    Add memory_profile module to record code memory  changes.

--- Python/Program/mt_process_and_mt_thread_compare.py
+++ Python/Program/mt_process_and_mt_thread_compare.py
@@ -1,8 +1,11 @@
 import math
 from time import time
-from threading import Thread as Td
 import multiprocessing as mp
+from threading import Thread as Td
+from memory_profiler import profile
 
+
+@profile
 def calculate(num):
     total = 1
     for i in range(1, num):
@@ -11,6 +14,8 @@ def calculate(num):
         total += total
         print("%.2f" % total)
 
+
+@profile
 def circle_area(radius):
     for r in range(radius):
         area = r **2 -math.pi
@@ -20,15 +25,16 @@ def circle_area(radius):
 if __name__ == "__main__":
     # Main processing.
     start = time()
-    calculate(100000)
-    circle_area(100000)
+    calculate(1000)
+    circle_area(1000)
     end1 = time()-start
+
 
     # Multiple thread.
     start = time()
-    t1 = Td(target=calculate, args=(100000,))
+    t1 = Td(target=calculate, args=(1000,))
     t1.name = "t1-calculate"
-    t2 = Td(target=circle_area, args=(100000,))
+    t2 = Td(target=circle_area, args=(1000,))
     t2.name = "t2-circle_area"
 
     t1.start()
@@ -38,10 +44,11 @@ if __name__ == "__main__":
     t2.join()
     end2 = time()-start
 
+
     # Multiple processing.
     start = time()
-    p1 = mp.Process(target=calculate, args=(100000,))
-    p2 = mp.Process(target=circle_area, args=(100000,))
+    p1 = mp.Process(target=calculate, args=(1000,))
+    p2 = mp.Process(target=circle_area, args=(1000,))
 
     p1.start()
     p2.start()
@@ -52,7 +59,6 @@ if __name__ == "__main__":
     end3 = time()-start
 
     # Compare costing time.
-    print("Multiple thread cost time %.2fs" % end2)
     print("Main processing cost time %.2fs" % end1)
+    print("Multiple thread cost time %.2fs" % end2)
     print("Multiple processing cost time %.2fs" % end3)
-
