92dd9a687ca8a5dd9a1bc02ce88a58fbc55b83f9
Michael07220823 <michael31703@gmail.com>
2020-06-01 14:37:47 +0800

    Add program log record example.

---
+++ Python/Program/log.py
@@ -0,0 +1,14 @@
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M',
+                    handlers=[logging.FileHandler('my.log', 'w', 'utf-8'), ])
+
+
+logging.debug('Hello debug!')
+logging.info('Hello info!')
+logging.warning('Hello warning!')
+logging.error('Hello error!')
+logging.critical('Hello critical!')
