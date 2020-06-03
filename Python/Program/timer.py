02f9aa0b5727568f864c1ad8114d00a61d67c418
Overcomer <michael31703@gmail.com>
2020-06-03 12:14:10 +0800

    Add string to information of print.

--- Python/Program/timer.py
+++ Python/Program/timer.py
@@ -1,16 +1,18 @@
 import time
 
 class Timer(object):
+    def __init__(self, string):
+        self._string = string
+
     def __enter__(self):
         self._start = time.time()
 
     def __exit__(self, *args):
-        self._end = time.time()
-        self.secs = self._end - self._start
-        print("[INFO] time cost %.2fs" % self.secs)
+        self.secends = time.time() - self._start
+        print("[INFO] %s cost time %.4f secs." % (self._string, self.secends))
 
 
 if __name__ == "__main__":
-    with Timer() as t:
+    with Timer("Test") as t:
         for i in range(999):
             print(i)
