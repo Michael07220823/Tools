63919498832da3c07131f8c973e17e909d05498c
Michael07220823 <michael31703@gmail.com>
2020-10-23 21:18:22 +0800

    Update timer to calculate to hours.

--- Python/Program/timer.py
+++ Python/Program/timer.py
@@ -1,4 +1,5 @@
 import time
+import math
 
 class Timer(object):
     def __init__(self, string):
@@ -9,10 +10,24 @@ class Timer(object):
 
     def __exit__(self, *args):
         self.secends = time.time() - self._start
-        print("[INFO] %s cost time %.4f secs." % (self._string, self.secends))
+        
+        if self.secends/60 <1:
+            print("[INFO] %s cost time %.2f secs." % (self._string, self.secends))
+        elif self.secends/60 >= 1 and self.secends/3600 < 1:
+            self.minutes = math.floor(self.secends/60 )
+            self.secends = self.secends - self.minutes*60
+            print("[INFO] %s cost time %d mins %.2f secs." % (self._string, self.minutes ,self.secends))
+        elif self.secends/3600 >= 1 :
+            self.hours = math.floor(self.secends/3600)
+            self.minutes = math.floor((self.secends - self.hours*3600) / 60)
+            self.secends = self.secends - self.hours*3600 - self.minutes*60
+            print("[INFO] %s cost time %d hours %d mins %.2f secs." % (self._string, self.hours, self.minutes ,self.secends))
 
 
 if __name__ == "__main__":
-    with Timer("Test") as t:
-        for i in range(999):
-            print(i)
+    with Timer("Test"):
+        count = 0
+
+        while True:
+            count+=1
+            print("%d" % count)
